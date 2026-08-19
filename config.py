2d30f9fdea0f1a48921b7ed2ea190316ad25cc86
deswalumesh80-sys <deswalumesh80@gmail.com>
2026-08-19 14:42:58 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", "38398715"))
+API_HASH = os.environ.get("API_HASH", "6cf70e41fbc67908ae547a31c2cfe9c3a")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8957794036:AAHJctqtHq5zMKke_t9kJqC-Kx8aNqyGW8c")
+PORT = int(os.environ.get("PORT", 8080))
